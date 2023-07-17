2285de35329c75f39786ce2ce9f0ff1df2941495
robert.bruckbauer <robert.bruckbauer@e-mundo.de>
2023-07-17 18:04:53 +0200

    spotless

--- app/demo/src/main/java/esy/demo/EsyDemoConfiguration.java
+++ app/demo/src/main/java/esy/demo/EsyDemoConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 /**
--- app/demo/src/main/java/esy/demo/EsyDemoRunner.java
+++ app/demo/src/main/java/esy/demo/EsyDemoRunner.java
@@ -3,7 +3,6 @@ package esy.demo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.ComponentScan;
 
 @Slf4j
 @SpringBootApplication
